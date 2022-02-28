98971fd593c2abd388f25b12bb558b99d062173d
Caleb Ricks <99217033+Skultanix@users.noreply.github.com>
2022-02-28 14:45:04 -0700

    Server now listening on port 4000.

--- index.js
+++ index.js
@@ -6,3 +6,8 @@ const app = express()
 
 //dictating that JSON objects are used in server response
 app.use(express.json())
+
+//dictating that server listen on port 4000
+app.listen(4000, () => {
+    console.log(`Server running on port 4000.`)
+})
