6cba740c50c20b2f454ac35d52363abbca576b52
Caleb Ricks <99217033+Skultanix@users.noreply.github.com>
2022-02-28 14:53:42 -0700

    Endpoints created.

--- index.js
+++ index.js
@@ -7,6 +7,17 @@ const app = express()
 //dictating that JSON objects are used in server response
 app.use(express.json())
 
+//creation of endpoints
+app.get(`/api/users`, (req, res) => {
+    let friends = [`Nitin`, `Eric`, `Jeddy`, `Cameron`, `Riley`]
+    return res.status(200).send(friends)
+})
+
+app.get(`/weather/:temperature`, (req, res) => {
+    const phrase = `<h3>It was ${req.params.temperature} today.</h3>`;
+    return res.status(200).send(phrase)
+})
+
 //dictating that server listen on port 4000
 app.listen(4000, () => {
     console.log(`Server running on port 4000.`)
